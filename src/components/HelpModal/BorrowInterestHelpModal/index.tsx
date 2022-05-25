842f79802f58d0e23f1db093f24ed1cc1c0ee369
keeplearning <steve@lit.tech>
2022-05-25 23:12:24 +0800

    fix: remove unused fragment

--- src/components/HelpModal/BorrowInterestHelpModal/index.tsx
+++ src/components/HelpModal/BorrowInterestHelpModal/index.tsx
@@ -1,25 +1,6 @@
-import React from 'react';
-import { useIntl } from 'react-intl';
-
-import { useThemeContext } from '@pret/pret-ui-kit';
-import HelpItem from '../../HelpItem';
-import TextWithModal from '../../TextWithModal';
-
-import messages from './messages';
-
 import { HelpModalProps } from '../types';
 
-export default function BorrowInterestHelpModal({
-  text,
-  iconSize,
-  className,
-  lightWeight,
-  color,
-  onWhiteBackground,
-}: HelpModalProps) {
-  const intl = useIntl();
-  const { currentTheme } = useThemeContext();
-
+export default function BorrowInterestHelpModal({ text }: HelpModalProps) {
   return (
     <div className="BorrowInterestHelpModal">
       {text}
@@ -29,42 +10,5 @@ export default function BorrowInterestHelpModal({
         }
       `}</style>
     </div>
-  );
-  return (
-    <TextWithModal
-      text={text}
-      iconSize={iconSize}
-      className={className}
-      color={color}
-      lightWeight={lightWeight}
-      onWhiteBackground={onWhiteBackground}
-    >
-      <HelpItem
-        caption={intl.formatMessage(messages.firstHelpItemCaption)}
-        description={
-          <p className="BorrowInterestHelpModal__text">
-            {intl.formatMessage(messages.firstHelpItemDescription, {
-              stay: (
-                <span style={{ color: `${currentTheme.primary.hex}` }}>
-                  {intl.formatMessage(messages.stayTheSame)}
-                </span>
-              ),
-            })}
-          </p>
-        }
-        captionColor="primary"
-      />
-      <HelpItem
-        caption={intl.formatMessage(messages.secondHelpItemCaption)}
-        description={intl.formatMessage(messages.secondHelpItemDescription, {
-          fluctuate: (
-            <span style={{ color: `${currentTheme.secondary.hex}` }}>
-              {intl.formatMessage(messages.fluctuate)}
-            </span>
-          ),
-        })}
-        captionColor="secondary"
-      />
-    </TextWithModal>
   );
 }
