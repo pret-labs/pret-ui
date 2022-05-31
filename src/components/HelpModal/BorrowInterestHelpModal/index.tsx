01a2a3769889222fee412b7f3303c61c981cdbef
keep learning <99638968+keeplearning-near@users.noreply.github.com>
2022-05-31 17:18:08 +0800

    fix: hide stable borrow, change translation and remove tip modal (#14)
    * fix: change translation and remove tip modal
    
    * fix: remove stable borrow apy
    
    * fix: comment unused fragment
    
    * fix: comment stable borrowing apy card

--- src/components/HelpModal/BorrowInterestHelpModal/index.tsx
+++ src/components/HelpModal/BorrowInterestHelpModal/index.tsx
@@ -1,60 +1,69 @@
-import React from 'react';
-import { useIntl } from 'react-intl';
+// import React from 'react';
+// import { useIntl } from 'react-intl';
 
-import { useThemeContext } from '@pret/pret-ui-kit';
-import HelpItem from '../../HelpItem';
-import TextWithModal from '../../TextWithModal';
+// import { useThemeContext } from '@pret/pret-ui-kit';
+// import HelpItem from '../../HelpItem';
+// import TextWithModal from '../../TextWithModal';
 
-import messages from './messages';
+// import messages from './messages';
 
 import { HelpModalProps } from '../types';
 
-export default function BorrowInterestHelpModal({
-  text,
-  iconSize,
-  className,
-  lightWeight,
-  color,
-  onWhiteBackground,
-}: HelpModalProps) {
-  const intl = useIntl();
-  const { currentTheme } = useThemeContext();
+export default function BorrowInterestHelpModal({ text }: HelpModalProps) {
+  // iconSize,
+  // className,
+  // lightWeight,
+  // color,
+  // onWhiteBackground,
+  // }: HelpModalProps) {
+  // const intl = useIntl();
+  // const { currentTheme } = useThemeContext();
 
   return (
-    <TextWithModal
-      text={text}
-      iconSize={iconSize}
-      className={className}
-      color={color}
-      lightWeight={lightWeight}
-      onWhiteBackground={onWhiteBackground}
-    >
-      <HelpItem
-        caption={intl.formatMessage(messages.firstHelpItemCaption)}
-        description={
-          <p className="BorrowInterestHelpModal__text">
-            {intl.formatMessage(messages.firstHelpItemDescription, {
-              stay: (
-                <span style={{ color: `${currentTheme.primary.hex}` }}>
-                  {intl.formatMessage(messages.stayTheSame)}
-                </span>
-              ),
-            })}
-          </p>
+    <div className="BorrowInterestHelpModal">
+      {text}
+      <style jsx>{`
+        .BorrowInterestHelpModal {
+          font-size: 12px;
         }
-        captionColor="primary"
-      />
-      <HelpItem
-        caption={intl.formatMessage(messages.secondHelpItemCaption)}
-        description={intl.formatMessage(messages.secondHelpItemDescription, {
-          fluctuate: (
-            <span style={{ color: `${currentTheme.secondary.hex}` }}>
-              {intl.formatMessage(messages.fluctuate)}
-            </span>
-          ),
-        })}
-        captionColor="secondary"
-      />
-    </TextWithModal>
+      `}</style>
+    </div>
   );
+  // return (
+  //   <TextWithModal
+  //     text={text}
+  //     iconSize={iconSize}
+  //     className={className}
+  //     color={color}
+  //     lightWeight={lightWeight}
+  //     onWhiteBackground={onWhiteBackground}
+  //   >
+  //     <HelpItem
+  //       caption={intl.formatMessage(messages.firstHelpItemCaption)}
+  //       description={
+  //         <p className="BorrowInterestHelpModal__text">
+  //           {intl.formatMessage(messages.firstHelpItemDescription, {
+  //             stay: (
+  //               <span style={{ color: `${currentTheme.primary.hex}` }}>
+  //                 {intl.formatMessage(messages.stayTheSame)}
+  //               </span>
+  //             ),
+  //           })}
+  //         </p>
+  //       }
+  //       captionColor="primary"
+  //     />
+  //     <HelpItem
+  //       caption={intl.formatMessage(messages.secondHelpItemCaption)}
+  //       description={intl.formatMessage(messages.secondHelpItemDescription, {
+  //         fluctuate: (
+  //           <span style={{ color: `${currentTheme.secondary.hex}` }}>
+  //             {intl.formatMessage(messages.fluctuate)}
+  //           </span>
+  //         ),
+  //       })}
+  //       captionColor="secondary"
+  //     />
+  //   </TextWithModal>
+  // );
 }
